b4ed5aee99ec2e13ebe1c3da9560505327ad5e12
Jonathan Pavkov <jonathan.pavkov@gmail.com>
2024-01-30 15:24:19 -0500

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    "articles",
 ]
+
+TIME_ZONE = "America/New_York"  # new
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
